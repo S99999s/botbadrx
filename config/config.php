717abe8483bc28ff0c92484ac12442b7ca6a5df9
badrx <badreddinnakhil@gmail.com>
2023-03-13 02:19:37 +0100

    primer commit

---
+++ config/config.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Bot Token
+|--------------------------------------------------------------------------
+|
+| Change this to your Bot API Token
+| It can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['botToken'] = "6065564812:AAHBMgW2EOJTmdDADEkdaPemjre_-tOBVkM";
+
+/*
+|--------------------------------------------------------------------------
+| Admin User ID
+|--------------------------------------------------------------------------
+|
+| Change this to Admin's Numeric User ID
+| ID can be obtained from https://telegram.dog/username_to_id_bot
+|
+*/
+$config['adminID'] = "5259818746";
+
+/*
+|--------------------------------------------------------------------------
+| Logs Channel ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['logsID'] =  "-1001773178481";
+
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Current timezone for Logging Activities with time
+| It can be obtained from http://1min.in/content/international/time-zones
+| By Default it's in IST
+|
+*/
+$config['timeZone'] =  "Asia/Kolkata";
+
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+| Database to Store User Data
+|
+*/
+$config['db']['hostname'] =  "sql7.freesqldatabase.com";
+$config['db']['username'] =  "sql7604117";
+$config['db']['password'] =  "1M7u5VEENx";
+$config['db']['database'] =  "sql7604117";
+
+/*
+|--------------------------------------------------------------------------
+| Anti-Spam Timer
+|--------------------------------------------------------------------------
+|
+| Anti-Spam Timer to prevent Spammers from Spamming the Checker
+| Value is in Seconds. "20" = 20seconds
+|
+*/
+$config['anti_spam_timer'] =  "20";
+
+/*
+|--------------------------------------------------------------------------
+| SK Keys
+|--------------------------------------------------------------------------
+|
+| SK Keys for !sm checker gate
+| Add a Live SK Key here. You can Also add Multiple SK Keys
+| array('sk1','sk2','sk3')
+|
+*/
+$config['sk_keys'] =  array('sk_live_69GKI0saLB8uIEnxzv8VTvRX','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');
+?>
